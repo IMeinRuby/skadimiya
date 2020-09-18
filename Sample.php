d82f3ffd9ee4fe646cefb539cb6fa63bb10fd0fa
Skadimiya <46038067+Skadimiya@users.noreply.github.com>
2020-09-18 13:34:28 +0800

    Sample Project for CS51

---
+++ Sample.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TCC Online Learning - Homepage</title>
+	<link rel="stylesheet" type="text/css" href="css/design.css">
+</head>
+	<form action="Sample.php" method="POST">
+	
+	<body>
+	<header>
+		<div class="logo">
+			<img src="logo.jpg" width="80" height="80"> <h1 class="title"> TOMAS CLAUDIO COLLEGES </h1>
+		</div>
+
+		<div class="header1">
+			<h1 class="welcome">Welcome to TCC Online Learning!</h1>
+			<h2 class="desc1">This is a free educational website dedicated solely to tomas claudio colleges that <br> offers classroom contents, 
+			communication, collaboration, and coaching platform to <br> K-12 students, college students and teachers.</br></h2>
+		</div>
+
+		<div class="header2">
+			<h1 class="tools">The website offers various tools and <br> utilities to aid the professors with <br> their workload!
+			</h1>
+			<h2 class="desc2">All the necessities required to get the job done!</h2>
+		</div>
+
+		<div class="header3">
+			<h1 class="students">Encouraging students to cooperate <br> and comply on time with our <br> user-friendly UI!</h1>
+			<h2 class="desc3">Students will have an easier time browsing through our website with a simple design and <br> interface!</h2>
+		</div>
+
+		<img class="pic" src="back.jpg" width="600" height="700">	
+		<ul>
+			<li class="account">
+			<a href="Sample2.php">Login</a>
+			<a href='#'>Sign Up</a>
+			</li>
+		</ul>	
+
+
+	</header>
+	</body>
+
+</html>
---
+++ Sample2.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TCC Online Learning - Login</title>
+	<link rel="stylesheet" type="text/css" href="css/design.css">
+</head>
+	<form action="Sample.php" method="POST">
+	
+	<body>
+	<header>
+		<div class="logo">
+			<img src="logo.jpg" width="80" height="80"> <h1 class="title"> TOMAS CLAUDIO COLLEGES </h1>
+		</div>
+
+		<div class="loginform">
+			<table border=1 cellspacing="20" width="600">
+				<tr>
+					<td align="center">First Name:</td>
+					<td><input type="text" name="fn" size=40></td>
+				</tr>
+				<tr>
+					<td align="center">Last Name:</td>
+					<td><input type="text" name="ln" size="40"></td>
+				</tr>
+				<tr>
+					<td align="center">Email address:</td>
+					<td><input type="text" name="email" size="60"></td>
+				</tr>
+
+		</div>
+	</header>
+	</body>
+</html>
